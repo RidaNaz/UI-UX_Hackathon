176e5fd0755c072f87affcceb6178165aecc4282
RidaNaz <rnaz3414@gmail.com>
2024-12-12 16:37:09 +0500

    Commit by Rida

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,109 +1,11 @@
-import { team } from "@/helper";
-import Image from "next/image";
+import AboutUs from "@/components/about/AboutUs";
+import Companies from "@/components/about/Companies";
 
 export default function About() {
     return (
-        <div className="min-h-screen px-6 sm:px-8 lg:px-32 md:px-16">
-            <div className="container mx-auto px-4 grid md:grid-cols-2 items-center justify-between mt-12">
-                {/* Text Section */}
-                <div className="lg:w-2/3 text-center md:text-left">
-                    <h3 className="text-[#252B42] font-bold text-[16px] uppercase tracking-tiny sm:block hidden">
-                        About Company
-                    </h3>
-                    <h1 className="md:text-[58px] sm:text-[48px] text-[40px] uppercase font-bold text-[#252B42] tracking-md mt-2">
-                        About Us
-                    </h1>
-                    <p className="text-[#737373] tracking-md text-[20px] mt-4 font-bold">
-                        We know how large objects will act, but things on a small scale.
-                    </p>
-                    <a
-                        href="/quote"
-                        className="mt-6 inline-block bg-[#23A6F0] text-white px-11 py-4 rounded-md shadow-lg hover:bg-blue-600 transition duration-300 tracking-md text-[14px] font-bold"
-                    >
-                        Get Quote Now
-                    </a>
-                </div>
-
-                {/* Image Section */}
-                <div className=" flex justify-center md:justify-end">
-                    <div className="relative">
-                        <div className="absolute top-12 -left-6 bg-pink-100 w-80 h-80 rounded-full mix-blend-multiply"></div>
-                        <Image
-                            src="/aboutimage.png"
-                            alt="Shopping Woman"
-                            width={400}
-                            height={400}
-                            className="relative z-10"
-                        />
-                    </div>
-                </div>
-            </div>
-
-            <div className="md:text-left text-center">
-                <p className="text-[#E74040] text-[14px] tracking-md font-bold mt-12">Problems trying</p>
-                <div className="grid md:grid-cols-2 justify-items-center mt-6 gap-10">
-                    <div className="font-bold">
-                        <h3 className="text-[#252B42] text-[24px] tracking-tiny">Met minim Mollie non desert Alamo est sit cliquey dolor do met sent.</h3>
-                    </div>
-                    <div>
-                        <p className="tracking-md text-[14px] text-[#737373] font-bold">Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics </p>
-                    </div>
-                </div>
-            </div>
-
-            <div className="grid sm:grid-cols-2 lg:grid-cols-4 font-bold mt-32 gap-14">
-                <div className="grid justify-items-center">
-                    <h3 className="text-[#252B42] tracking-md md:text-[58px] sm:text-[48px] text-[40px]">15K</h3>
-                    <p className="text-[#737373] tracking-tiny text-[16px]">Happy Customers</p>
-                </div>
-                <div className="grid justify-items-center">
-                    <h3 className="text-[#252B42] tracking-md md:text-[58px] sm:text-[48px] text-[40px]">150K</h3>
-                    <p className="text-[#737373] tracking-tiny text-[16px]">Monthly Visitors</p>
-                </div>
-                <div className="grid justify-items-center">
-                    <h3 className="text-[#252B42] tracking-md md:text-[58px] sm:text-[48px] text-[40px]">15</h3>
-                    <p className="text-[#737373] tracking-tiny text-[16px]">Countries  Worldwide</p>
-                </div>
-                <div className="grid justify-items-center">
-                    <h3 className="text-[#252B42] tracking-md md:text-[58px] sm:text-[48px] text-[40px]">100+</h3>
-                    <p className="text-[#737373] tracking-tiny text-[16px]">Top Partners</p>
-                </div>
-            </div>
-
-            <div className="font-bold mt-32">
-                <div className="grid justify-items-center">
-                    <h2 className="text-[40px] text-[#252B42] tracking-md text-center">Meet Our Team</h2>
-                    <p className="text-[#737373] text-[14px] tracking-md md:w-1/2 text-center mt-2">Problems trying to resolve the conflict between
-                        the two major realms of Classical physics: Newtonian mechanics </p>
-                </div>
-
-                {/* Team Grid */}
-                <div className="grid md:grid-cols-3 gap-10 mb-6 w-full mt-20">
-                    {team.map((teamMember) => (
-                        <div key={teamMember.id}>
-                            <Image
-                                src={teamMember.image}
-                                alt={teamMember.name}
-                                width={1000}
-                                height={1000}
-                                className="w-full h-64 object-cover"
-                            />
-
-                            <div className="font-bold mt-10 text-center grid justify-items-center">
-                                <h1 className=" text-[#252B42] tracking-tiny text-[16px]">{teamMember.name}</h1>
-                                <p className="text-[#737373] tracking-md text-[14px] mt-2">{teamMember.profession}</p>
-
-                                <div className="flex gap-4 mt-3 items-center">
-                                    <Image src="/fb2.svg" alt="Facebook" width={21} height={21} />
-                                    <Image src="/insta2.svg" alt="Instagram" width={18} height={18} />
-                                    <Image src="/twitter3.svg" alt="Twitter" width={20} height={20} />
-                                </div>
-                            </div>
-                        </div>
-
-                    ))}
-                </div>
-            </div>
+        <div>
+            <AboutUs />
+            <Companies />
         </div>
-    );
+    )
 }
---
+++ src/components/about/Companies.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Logos from "../sub/Logos";
+
+export default function Companies() {
+    return (
+        <div className="mt-32 bg-[#FAFAFA] ">
+            <div className="font-bold grid justify-items-center text-center py-10">
+                <h2 className="text-[40px] tracking-md text-[#252B42] pt-12">Big Companies Are Here</h2>
+                <p className="text[14px] text-[#737373] tracking-md font-normal w-[80%] md:w-1/2 mt-5 px-4">Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics </p>
+            </div>
+            <div className="pb-10">
+                <Logos />
+            </div>
+
+
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 mt-10">
+                <div className="bg-[#2A7CC7] lg:col-span-2 lg:px-40 py-32 font-bold text-white md:text-left text-center justify-items-center lg:justify-items-start grid">
+                    <h3 className="text-[16px] tracking-tiny">WORK WITH US</h3>
+                    <h1 className="tracking-md text-[40px] mt-4 px-4">Now Let&apos;s grow Yours</h1>
+                    <p className="font-normal mt-4 tracking-md w-[80%] text-center lg:text-left">The gradual accumulation of information about atomic and small-scale behavior during the first quarter of the 20th </p>
+                    <button className="mt-4 px-10 py-3 font-bold text-[14px] w-fit border border-white rounded-md tracking-md">
+                        Button
+                    </button>
+                </div>
+                <div>
+                    <Image
+                        src="/aboutimage2.jpg"
+                        alt="Shopping Woman"
+                        width={2000}
+                        height={2000}
+                        className="object-cover"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
